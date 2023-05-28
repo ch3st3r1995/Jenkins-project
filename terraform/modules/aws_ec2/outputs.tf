9c46927827cb4e2b11080cb4d22c000d3a06f57b
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-28 12:18:19 -0500

    Update 10 Pipeline

--- terraform/modules/aws_ec2/outputs.tf
+++ terraform/modules/aws_ec2/outputs.tf
@@ -2,3 +2,12 @@ output "id" {
   value = aws_instance.instance.id
 }
 
+output "ec2_instance1_value" {
+  value = aws_instance.ec2_instance1.value
+}
+
+output "ec2_instance2_value" {
+  value = aws_instance.ec2_instance2.value
+}
+
+
