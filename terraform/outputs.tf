02216850883209d46a60667853a987c20985f61a
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 11:10:55 -0500

    Update 10 Pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,4 @@
-output "ec2_instance1_output" {
-  value = terraform output --json | jq .ec2_instance1.value -r
+output "ec2_instance_ip" {
+  value = aws_instance.ec2_instance.public_ip
 }
 
-output "ec2_instance2_output" {
-  value = terraform output --json | jq .ec2_instance2.value -r
-}
