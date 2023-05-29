bb75f2296c842991b403e584457184b85c5de455
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 11:07:49 -0500

    Update 10 Pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_instance1_output" {
-  value = exec("terraform output --json | jq .ec2_instance1.value -r")
+  value = "terraform output --json | jq .ec2_instance1.value -r"
 }
 
 output "ec2_instance2_output" {
-  value = exec("terraform output --json | jq .ec2_instance2.value -r")
+  value = "terraform output --json | jq .ec2_instance2.value -r"
 }
