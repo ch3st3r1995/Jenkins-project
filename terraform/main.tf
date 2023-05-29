789621bfabd1b3555f06ac6498a280ada9cbee70
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 12:15:02 -0500

    Update 10 Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -227,7 +227,7 @@ module "ec2_instance1" {
 }
 
 output "ec2_instance1_public_ip" {
-  value = module.ec2_instance1.public_ip
+  value = module.ec2_instance1.ec2_instance_public_ip
 }
 
 
@@ -240,7 +240,7 @@ module "ec2_instance2" {
 }
 
 output "ec2_instance2_public_ip" {
-  value = module.ec2_instance2.public_ip
+  value = module.ec2_instance2.ec2_instance_public_ip
 }
 
 
