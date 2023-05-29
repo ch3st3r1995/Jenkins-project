cc77cf01ffa47ce506f34db3b2cb6bbd94360bcb
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 15:50:24 -0500

    Update 10 Pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_instance1" {
-  value = module.ec2_instance1.instance_public_ip
+  value = module.ec2_instance1.public_ip
 }
 
 output "ec2_instance2" {
-    value = module.ec2_instance2.instance_public_ip
+    value = module.ec2_instance2.public_ip
 }
 
 output "rds_endpoint" {
