8a99fd49c9a0e7f84bce7c99dc29d69b2e532024
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 15:07:01 -0500

    Update 10 Pipeline

--- terraform/handle_output.sh
+++ terraform/handle_output.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-terraform output --json | jq .ec2_instance1.value -r >> ../ansible/inventory/hosts
-terraform output --json | jq .ec2_instance2.value -r >> ../ansible/inventory/hosts
+echo$(terraform output -raw ec2_instance1_public_ip) >>../ansible/inventory/hosts
+echo$(terraform output -raw ec2_instance2_public_ip) >>../ansible/inventory/hosts
+# terraform output --json | jq .ec2_instance1.value -r >> ../ansible/inventory/hosts
+# terraform output --json | jq .ec2_instance2.value -r >> ../ansible/inventory/hosts
 rdsHost=$(terraform output --json | jq .rds_endpoint.value -r |cut -d ':' -f 1)
 echo $rdsHost
 sed -i "s/rds_host/$rdsHost/g" ../ansible/roles/wordpress/defaults/main.yml
