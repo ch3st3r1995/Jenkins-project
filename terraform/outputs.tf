1501a41b32864bdbea61d19eed68cc58dbd016d8
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-29 12:07:37 -0500

    Update 10 Pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,15 @@
-# output "ec2_instance1_public_ip" {
-#   value = module.ec2_instance1.ec2_instance_public_ip
-# }
+data "aws_instance" "ec2_instance1" {
+  instance_id = aws_instance.ec2_instance1.id
+}
 
-# output "ec2_instance2_public_ip" {
-#   value = module.ec2_instance2.ec2_instance_public_ip
-# }
+data "aws_instance" "ec2_instance2" {
+  instance_id = aws_instance.ec2_instance2.id
+}
+
+output "ec2_instance1_public_ip" {
+  value = data.ec2_instance1.public_ip
+}
+
+output "ec2_instance2_public_ip" {
+  value = data.ec2_instance2.public_ip
+}
