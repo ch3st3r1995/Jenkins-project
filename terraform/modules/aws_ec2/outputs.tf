2728d26f1e78614f8c5e88a5ad34040df2b687b1
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-30 16:40:16 -0500

    Update 11 Pipeline

--- terraform/modules/aws_ec2/outputs.tf
+++ terraform/modules/aws_ec2/outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = aws_instance.instance.id
 }
+
+output "public_ip" {
+  value = aws_instance.instance.public_ip
+}
--- terraform/modules/aws_rds/outputs.tf
+++ terraform/modules/aws_rds/outputs.tf
@@ -1,3 +1,8 @@
 output "id" {
   value = aws_db_instance.db.id
 }
+
+
+output "endpoint" {
+  value = aws_db_instance.db.endpoint
+}
