78dfb8f20e7928b1dfd1c083ff1477d3ef3c867e
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-30 22:37:24 -0500

    Update 12

--- terraform/handle_output.sh
+++ terraform/handle_output.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-terraform output --json | jq .ec2_instance1.value -r >> ../ansible/inventory/hosts
-terraform output --json | jq .ec2_instance2.value -r >> ../ansible/inventory/hosts
-ec2_instance1_public_ip=$(terraform output --json | jq .ec2_instance1.value -r)
-ec2_instance2_public_ip=$(terraform output --json | jq .ec2_instance2.value -r)
+terraform output --json | jq .ec2_instance1_public_ip.value -r >> ../ansible/inventory/hosts
+terraform output --json | jq .ec2_instance2_public_ip.value -r >> ../ansible/inventory/hosts
+ec2_instance1_public_ip=$(terraform output --json | jq .ec2_instance1_public_ip.value -r)
+ec2_instance2_public_ip=$(terraform output --json | jq .ec2_instance2_public_ip.value -r)
 echo $ec2_instance1_public_ip
 echo $ec2_instance2_public_ip
 rdsHost=$(terraform output --json | jq .rds_endpoint.value -r |cut -d ':' -f 1)
