cc85e4b25197e1cc01a84b18f1f610a54a97327f
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-31 17:52:01 -0500

    Update 12

--- terraform/handle_output.sh
+++ terraform/handle_output.sh
@@ -6,6 +6,8 @@ ec2_instance1_public_ip=$(terraform output --json | jq .ec2_instance1_public_ip.
 ec2_instance2_public_ip=$(terraform output --json | jq .ec2_instance2_public_ip.value -r)
 echo $ec2_instance1_public_ip
 echo $ec2_instance2_public_ip
+rdsHost=$(terraform output --json | jq .rds_endpoint.value -r | cut -d ':' -f 1)
+echo $rdsHost
 sed -i "s/rds_host/$rdsHost/g" ../ansible/roles/wordpress/defaults/main.yml
 cat ../ansible/roles/wordpress/defaults/main.yml
 rdsPassword=$(aws secretsmanager get-secret-value --secret-id rds-credentials --region us-east-1 | jq .SecretString | cut -d ':' -f 2 | tr -d '"''\\''}')
