ddf69799a74799a801211a5551fcf0bfc7cb9de3
Serhii Nazarenko <sergiy_nazarenko@hotmail.com>
2023-05-30 22:15:49 -0500

    Update 12

--- terraform/handle_output.sh
+++ terraform/handle_output.sh
@@ -2,6 +2,10 @@
 
 terraform output --json | jq .ec2_instance1_public_ip.value -r >> ../ansible/inventory/hosts
 terraform output --json | jq .ec2_instance2_public_ip.value -r >> ../ansible/inventory/hosts
+ec2_instance1_public_ip=$(terraform output --json | jq .ec2_instance1_public_ip.value -r)
+ec2_instance2_public_ip=$(terraform output --json | jq .ec2_instance2_public_ip.value -r)
+echo $ec2_instance1_public_ip
+echo $ec2_instance2_public_ip
 rdsHost=$(terraform output --json | jq .rds_endpoint.value -r |cut -d ':' -f 1)
 echo $rdsHost
 sed -i "s/rds_host/$rdsHost/g" ../ansible/roles/wordpress/defaults/main.yml
